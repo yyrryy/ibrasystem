64b25d9379d2fed186ab9f104b5f8b6ae8719a24
Your Name <you@example.com>
2025-12-31 15:42:55 +0100

    'reer'

--- ibra/settings.py
+++ ibra/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
